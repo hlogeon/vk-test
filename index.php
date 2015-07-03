a0e0feb57d04514774bfee932337c8711c206481
Andrey Degtyaruk <hlogeon1@gmail.com>
2015-07-03 16:35:33 +0700

    Composer added to enable application on heroku

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
  * Entry point of application, the only one thing we do here is calling function from router.php which
  * will handle user request.
  */
+error_reporting(0);
+
 require_once(__DIR__.'/config.php');
 require_once(__DIR__.'/database.php');
 require_once(__DIR__.'/router.php');
